c531b857b5b361070d9beb5a6a5b8f70edbbd55a
Camelia Azzahra <98297776+CameliaAz@users.noreply.github.com>
2022-03-28 21:05:08 +0700

    Add files via upload

---
+++ Hitung.java
@@ -0,0 +1,10 @@
+package overloading2;
+
+public class Hitung {
+    public static void main(String[] args) {
+        BangunDatar hitunglah = new BangunDatar();
+
+        hitunglah.luas(2, 5, 6);
+        hitunglah.luas(3.5, 6.9);
+    }
+}
